c941975fafa45e9fc9db080969c939693aba951c
Nicole Dow <nikki.dow71@gmail.com>
2019-08-14 14:20:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,8 @@ class Student
           FROM students
           WHERE grade != 12
           SQL
-          
+     
+    #returns an array of students created from database:     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
