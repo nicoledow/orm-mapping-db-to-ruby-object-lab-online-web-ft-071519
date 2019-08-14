d4d964ee04bf427f9257025961f9f22dfaf46614
Nicole Dow <nikki.dow71@gmail.com>
2019-08-14 14:26:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -109,7 +109,7 @@ class Student
           LIMIT 1
           SQL
           
-    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
   end
   
 end
