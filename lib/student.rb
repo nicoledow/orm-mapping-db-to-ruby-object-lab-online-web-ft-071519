2180fe8dbb68ccb8f662cf870e8b7c98b0756be6
Nicole Dow <nikki.dow71@gmail.com>
2019-08-14 14:27:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -112,7 +112,7 @@ class Student
     DB[:conn].execute(sql).map {|row| self.new_from_db(row)}.first
   end
   
-  def self.all_students_in_grade_x(x)
+  def self.all_students_in_grade_X(x)
     
   end
   
